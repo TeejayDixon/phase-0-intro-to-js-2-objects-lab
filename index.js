9574ca241d462d8811e464266bd4abe7025de6cb
Teejay Dixon <tahjgdixon@gmail.com>
2022-08-14 16:58:06 -0700

    commit tree f811d56b7c0963d7d0902faaa9162d53156298c7

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+const employee = { 
+  name: 'Teejay',
+  streetAddress: '319 Pennsylvania Avenue'
+};
+
+
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+  const newEmployee = { ...employee };
+  
+  newEmployee.name = 'Sam';
+  newEmployee.streetAddress = '11 Broadway';
+
+  return newEmployee;
+ 
+  
+};
+  
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+   
+  employee["name"] = "Sam";
+  employee["streetAddress"] = "12 Broadway";
+    
+  return employee;
+
+
+};
+
+function deleteFromEmployeeByKey(employee, name, streetAddress) {
+  const deletedEmployee = { ...employee };
+
+
+  delete deletedEmployee.name;
+  delete deletedEmployee.streetAddress;
+
+  return deletedEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+  delete employee.name;
+
+  return employee;
+};
